5211f394ccc9c17c96e46a08fc6e4af3c290f72e
Kevin <kevin.vandenbreemen@gmail.com>
2025-10-04 10:17:05 -0700

    Get logging set up

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@ dependencies {
     // Pick a Ktor engine if you’ll use HTTP/SSE transports (optional for stdio)
     implementation("io.ktor:ktor-server-netty:2.3.12")
 
+    // SLF4J logging
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+
     testImplementation(kotlin("test"))
 }
 
